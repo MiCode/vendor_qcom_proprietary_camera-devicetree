2d77fbf7a964eb863a9ee983d86a1d94fe8ba72e
niyongqi <niyongqi@xiaomi.com>
2025-08-08 14:48:20 +0800

    Kernel: Xiaomi camera kernel devicetree changes for Xiaomi MIX Flip 2
    The Patch based on QualComm release TAG: LA.VENDOR.15.4.0.r1-10100-r1.0.r1_00025.1
    
    Signed-off-by: niyongqi <niyongqi@xiaomi.com>

---
+++ config/sun.mk
@@ -0,0 +1,10 @@
+dtbo-$(CONFIG_ARCH_SUN)   := sun-camera.dtbo
+dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
+				sun-camera-sensor-rumi.dtbo \
+				sun-camera-sensor-cdp.dtbo  \
+				sun-camera-sensor-qrd.dtbo  \
+				haotian-sm8750-camera-sensor.dtbo \
+				dada-sm8750-camera-sensor.dtbo \
+				miro-sm8750-camera-sensor.dtbo \
+				xuanyuan-sm8750-camera-sensor.dtbo \
+				bixi-sm8750-camera-sensor.dtbo
