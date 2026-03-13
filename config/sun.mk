4900e29d927aaca00f52fc51e07055197ca72569
niyongqi <niyongqi@xiaomi.com>
2026-03-13 19:52:04 +0800

    Kernel: Xiaomi camera devicetree changes for REDMI K90 / POCO F8 Pro Android W
    The Patch based on QualComm release TAG: LA.VENDOR.15.4.0.r1-15000-r1.0.r1_00044.1
    
    Signed-off-by: niyongqi <niyongqi@xiaomi.com>

---
+++ config/sun.mk
@@ -0,0 +1,12 @@
+dtbo-$(CONFIG_ARCH_SUN)   := sun-camera.dtbo
+dtbo-$(CONFIG_ARCH_SUN)   += sun-camera-sensor-mtp.dtbo \
+				sun-camera-sensor-rumi.dtbo \
+				sun-camera-sensor-cdp.dtbo  \
+				sun-camera-sensor-qrd.dtbo  \
+				haotian-sm8750-camera-sensor.dtbo \
+				dada-sm8750-camera-sensor.dtbo \
+				miro-sm8750-camera-sensor.dtbo \
+				xuanyuan-sm8750-camera-sensor.dtbo \
+				bixi-sm8750-camera-sensor.dtbo \
+				nirvana-sm8750-camera-sensor.dtbo \
+				annibale-sm8750-camera-sensor.dtbo
