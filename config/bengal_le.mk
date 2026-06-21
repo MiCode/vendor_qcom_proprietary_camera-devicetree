0ac87966b4aac94c9ecd96c5f1f13004c344ad15
niyongqi <niyongqi@xiaomi.com>
2026-06-21 14:58:47 +0800

    Kernel: Xiaomi camera devicetree changes for REDMI Pad 2 SE Android W
    The Patch based on QualComm release TAG: snapdragon-mid-2022-spf-2-0-2_00004.0
    
    Signed-off-by: niyongqi <niyongqi@xiaomi.com>

---
+++ config/bengal_le.mk
@@ -0,0 +1,9 @@
+dtbo-$(CONFIG_ARCH_KHAJE) := khaje-camera.dtbo
+dtbo-$(CONFIG_ARCH_KHAJE) += khajeg-camera-idp.dtbo \
+                                khaje-camera-sensor-idp.dtbo \
+                                khaje-camera-sensor-idp-nopmi.dtbo \
+                                khaje-camera-sensor-idps-display-90hz.dtbo \
+                                khaje-camera-sensor-idp-usbc.dtbo \
+
+dtbo-$(CONFIG_ARCH_SCUBA) += scuba-camera.dtbo
+
